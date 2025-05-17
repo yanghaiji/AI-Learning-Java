49f10cb567ed6a1114455dad628da2c7c6b2141f
yanghaiji <hai.ji.yang@ibm.com>
2025-05-17 15:02:35 +0800

    init code

---
+++ langchain4j-learning/src/main/java/com/javayh/langchain4j/learning/LangChain4JDemoApplication.java
@@ -0,0 +1,12 @@
+package com.javayh.langchain4j.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangChain4JDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangChain4JDemoApplication.class, args);
+    }
+}
